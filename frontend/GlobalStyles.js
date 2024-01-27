7bc184a6b748ae9c6b6cbee45133cdb35f15adbc
Max <maxlee10@g.ucla.edu>
2024-01-27 09:10:21 -0800

    updated global styling

---
+++ frontend/GlobalStyles.js
@@ -0,0 +1,21 @@
+
+  
+  /* font sizes */
+  export const FontSize = {
+    title1_size: 14,
+    title2_size: 12,
+    caption_size: 11,
+  };
+  /* Colors */
+  export const Color = {
+    lightGrey: "#f3f7ff",
+    colorWhite: "#fff",
+    dark: "#333",
+    primary: "#0063a4",
+  };
+  /* border radiuses */
+  export const Border = {
+    br_base: 16,
+    br_9xs: 4,
+    br_xs: 12,
+  };
