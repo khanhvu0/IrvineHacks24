1da9c78e6f6846163eea0052887389dced25e011
Max <maxlee10@g.ucla.edu>
2024-01-28 05:44:05 -0800

    static leaderboard

--- frontend/components/LeaderboardComp.js
+++ frontend/components/LeaderboardComp.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Color } from '../GlobalStyles';
@@ -6,7 +7,6 @@ export default function LeaderboardComp({ filepath, number, name, points }) {
   return (
     <View>
       {number === 1 ? (
-        /* number 1 */
         <View style={styles.one}>
           <Image
               source={filepath}
@@ -21,7 +21,6 @@ export default function LeaderboardComp({ filepath, number, name, points }) {
           <Text style={styles.pointsTextTop}>{points} points</Text>
         </View>
       ) : number === 2 ? (
-        /* number 2 */
     <View style={styles.horizontal}>
         <View style={styles.two}>
         <Image
@@ -36,7 +35,6 @@ export default function LeaderboardComp({ filepath, number, name, points }) {
         </View>
     </View>
       ) : number === 3 ? (
-        /* number 3 */
     <View style={styles.horizontal}>
         <View style={styles.three}>
         <Image
@@ -51,7 +49,6 @@ export default function LeaderboardComp({ filepath, number, name, points }) {
         </View>
     </View>
       ) : (
-        /* after number 3 */
         <View style={styles.box}>
           <View style={styles.horizontal}>
             <Text style={styles.nameText}><Text style={styles.numberText}>{number}. </Text> pic {name}</Text>
@@ -157,3 +154,177 @@ const styles = StyleSheet.create({
     //transform: [{ translateY: 10 }],
   },
 });
+*/
+
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Color } from '../GlobalStyles';
+export default function LeaderboardComp({ filepath, number, name, points }) {
+  return (
+    <View>
+      {number === 1 ? (
+        /* number 1 */
+        <View style={styles.one}>
+         <Image
+              source={filepath}
+              style={styles.profileImage}
+            />
+            <View style={styles.numberCirlceWrapper}>
+              <View style={styles.numberCircle}>
+              <Text style={styles.numberTextTop}>1</Text>
+            </View> 
+            </View>
+          <Text style={styles.nameTextTop}>{name}</Text>
+          <Text style={styles.pointsTextTop}>{points} Points</Text>
+        </View>
+      ) : number === 2 ? (
+        /* number 2 */
+    <View style={styles.horizontal}>
+        <View style={styles.two}>
+        <Image
+              source={filepath}
+              style={styles.profileImage}
+            />
+          <View style={styles.numberCircle}>
+              <Text style={styles.numberTextTop}>2</Text>
+            </View> 
+          <Text style={styles.nameTextTop}>{name}</Text>
+          <Text style={styles.pointsTextTop}>{points} Points</Text>
+        </View>
+    </View>
+      ) : number === 3 ? (
+        /* number 3 */
+    <View style={styles.horizontal}>
+        <View style={styles.three}>
+        <Image
+              source={filepath}
+              style={styles.profileImage}
+            />
+          <View style={styles.numberCircle}>
+              <Text style={styles.numberTextTop}>3</Text>
+            </View> 
+          <Text style={styles.nameTextTop}>{name}</Text>
+          <Text style={styles.pointsTextTop}>{points} Points</Text>
+        </View>
+    </View>
+      ) : (
+        /* after number 3 */
+        <View style={styles.box}>
+
+<View style={styles.evenHoriz}>
+            <View style={styles.horizontal}>
+              <Text style={styles.numberText}>{number}  </Text>
+
+              <Image
+                  source={filepath}
+                  style={styles.profileImageBoard}
+                /> 
+            </View>
+
+            <Text style={styles.nameText}>
+              {name}</Text>
+
+            <Text style={styles.pointsText}>{points} points</Text>
+
+          </View>
+
+        </View>
+      )}
+    </View>
+    );
+  }
+    
+
+  const styles = StyleSheet.create({
+    box: {
+      width: '95%',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 15,
+      backgroundColor: Color.white,
+      borderRadius: 10,
+      borderColor: '#c7c7c7',
+      borderWidth: 2,
+      fontSize: 50,
+      marginBottom: 2,
+      marginTop: 2,
+    },
+    one: {
+      alignItems: 'center',
+      transform: [{ translateY: 30 }],
+    },
+    two: {
+      left: '-60%',
+      alignItems: 'center',
+    },
+    three: {
+      left: '60%',
+      alignItems: 'center',
+    },
+    horizontal: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+    evenHoriz: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      width: '100%',
+    },
+    numberText: {
+      fontSize: 15,
+      fontWeight: 'bold',
+    },
+    nameText: {
+      fontSize: 15,
+      fontWeight: '300',
+    },
+    pointsText: {
+      fontSize: 15,
+      fontWeight: '600',
+    },
+    numberCircle: {
+      backgroundColor: Color.blue,
+      width: 30,
+      height: 30,
+      borderRadius: 15,
+      transform: [{ translateY: -12 }],
+    },
+    numberTextTop: {
+      fontSize: 20,
+      fontWeight: '800',
+      textAlign: 'center',
+      color: 'black',
+      transform: [{ translateY: 3 }],
+    },
+    nameTextTop: {
+      fontSize: 20,
+      fontWeight: '800',
+      color: 'black',
+      transform: [{ translateY: -10 }],
+    },
+    pointsTextTop: {
+      marginTop: 3,
+      fontSize: 12,
+      fontWeight: '400',
+      color: 'black',
+      transform: [{ translateY: -10 }],
+    },
+    profileImage: {
+      width: 60, // Set your desired width
+      height: 60, // Set your desired height
+      borderRadius: 30, // Half of the width and height to make it a circle
+      borderWidth: 3,
+      borderColor: Color.blue,
+    },
+    profileImageBoard: {
+      width: 40, // Set your desired width
+      height: 40, // Set your desired height
+      borderRadius: 20, // Half of the width and height to make it a circle
+      borderWidth: 3,
+      borderColor: Color.blue,
+      //transform: [{ translateY: 10 }],
+    },
+  });
--- frontend/screens/Leaderboard.js
+++ frontend/screens/Leaderboard.js
@@ -1,7 +1,10 @@
+/*
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from 'react'
 
 import LeaderboardComp from '../components/LeaderboardComp';
+import { Color } from '../GlobalStyles';
 
 export default function Leaderboard() {
   const [leaderboardData, setLeaderboardData] = useState([]);
@@ -40,6 +43,110 @@ export default function Leaderboard() {
       </View>
 
     </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Color.gray,
+    alignItems: 'center',
+    justifyContent: 'flex-end', // Align to the bottom of the screen
+    height: '100%', // Use 100% height
+  },
+  rows: {
+    backgroundColor: Color.blue,
+    alignItems: 'center',
+    width: '100%',
+    paddingBottom: 20,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  rows1: {
+    backgroundColor: Color.gray,
+    alignItems: 'center',
+    width: '100%',
+    paddingTop: 50,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  horiz: {
+    flexDirection: 'row',
+  }
+});
+
+/*
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Color.gray,
+    alignItems: 'center',
+    justifyContent: 'flex-end', // Align to the bottom of the screen
+    height: '100%', // Use 100% height
+  },
+  rows: {
+    backgroundColor: Color.blue,
+    alignItems: 'center',
+    width: '100%',
+    paddingBottom: 20,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  rows1: {
+    backgroundColor: Color.gray,
+    alignItems: 'center',
+    width: '100%',
+    paddingTop: 50,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  horiz: {
+    flexDirection: 'row',
+  }
+});
+*/
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Color } from '../GlobalStyles';
+
+import LeaderboardComp from '../components/LeaderboardComp';
+
+const person1 = require('../images/leaderboard/person1.png');
+const person2 = require('../images/leaderboard/person2.png');
+const person3 = require('../images/leaderboard/person3.png');
+const person4 = require('../images/leaderboard/person4.jpg');
+const person5 = require('../images/leaderboard/person5.png');
+const person6 = require('../images/leaderboard/person6.png');
+const person7 = require('../images/leaderboard/person7.png');
+const person8 = require('../images/leaderboard/person8.png');
+const person9 = require('../images/leaderboard/person9.png');
+const person10 = require('../images/leaderboard/person10.png');
+
+
+export default function Profile() {
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+    <View style={styles.rows1}>
+        <LeaderboardComp filepath={person1} number={1} name='Bob' points={99}/>
+        <View style={styles.horiz}>
+        <LeaderboardComp filepath={person2} number={2} name='Henry' points={85}/>
+        <LeaderboardComp filepath={person3} number={3} name='Max' points={69}/>
+        </View>
+      </View>
+      <View style={styles.rows}>
+        <LeaderboardComp filepath={person4} number={4} name='Adi' points={24}/>
+        <LeaderboardComp filepath={person5} number={5} name='Ewu' points={20}/>
+        <LeaderboardComp filepath={person6} number={6} name='Kon' points={13}/>
+        <LeaderboardComp filepath={person7} number={7} name='Irvine' points={10}/>
+        <LeaderboardComp filepath={person8} number={8} name='Hacks' points={7}/>
+        <LeaderboardComp filepath={person9} number={9} name='Is' points={3}/>
+        <LeaderboardComp filepath={person10} number={10} name='Cool' points={1}/>
+      </View>
+
+    </View>
+    </ScrollView>
   );
 }
 
